e2cb1be75b12deb13c10cb5246a231412d66291c
sitesoft1 <sitesoft1@gmail.com>
2020-10-07 23:14:49 +0300

    first commit 07102020

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ad-crew.ru/admin/');
+define('HTTP_CATALOG', 'http://ad-crew.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ad-crew.ru/admin/');
+define('HTTPS_CATALOG', 'http://ad-crew.ru/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\OpenServer2\OSPanel\domains\ad-crew.ru/admin/');
+define('DIR_SYSTEM', 'D:\OpenServer2\OSPanel\domains\ad-crew.ru/system/');
+define('DIR_IMAGE', 'D:\OpenServer2\OSPanel\domains\ad-crew.ru/image/');
+define('DIR_LANGUAGE', 'D:\OpenServer2\OSPanel\domains\ad-crew.ru/admin/language/');
+define('DIR_TEMPLATE', 'D:\OpenServer2\OSPanel\domains\ad-crew.ru/admin/view/template/');
+define('DIR_CONFIG', 'D:\OpenServer2\OSPanel\domains\ad-crew.ru/system/config/');
+define('DIR_CACHE', 'D:\OpenServer2\OSPanel\domains\ad-crew.ru/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:\OpenServer2\OSPanel\domains\ad-crew.ru/system/storage/download/');
+define('DIR_LOGS', 'D:\OpenServer2\OSPanel\domains\ad-crew.ru/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:\OpenServer2\OSPanel\domains\ad-crew.ru/system/storage/modification/');
+define('DIR_UPLOAD', 'D:\OpenServer2\OSPanel\domains\ad-crew.ru/system/storage/upload/');
+define('DIR_CATALOG', 'D:\OpenServer2\OSPanel\domains\ad-crew.ru/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ad_crew_ru');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
